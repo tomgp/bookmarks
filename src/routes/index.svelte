bb115e8be24663721df0d46c7cd04780704b0669
Tom P <tom.g.pearson@gmail.com>
2022-07-08 14:11:28 +0100

    adding svelte kit

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<h1>Links</h1>
+<main>
+<section>
+  <h2>Directory</h2>
+  <p>This is just a collection of websites I think are usefull, interesting or just generally good in some way.</p>
+</section>
+<section>
+  <h2>Articles</h2>
+  <p>This is a colleciton of archived articles I've read, kind of a traditional link log.</p>
+</section>
+</main>
+<style>
+  main{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+</style>
